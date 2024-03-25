9269f7923c4162bc4c74b9af78beeec50d7c629b
Sir-Tuc <155198681+CurtisLindahl@users.noreply.github.com>
2024-03-25 16:12:49 -0500

    vars

---
+++ backend.js
@@ -0,0 +1,30 @@
+var scoutname
+var temnumber
+var matchnumber
+var alliance
+
+var autoamppoints
+var autospeakerpoints
+var autoleftzone
+var teleamppoints
+var telespeakerpoints
+var telespeakeramplifiedpoints
+var telepasses
+
+var drops
+
+var climbed
+var parked
+
+var harmony
+var trap
+var numtraps
+var offeredcoop
+var didcoop
+
+var ampmike
+var sourcemike
+var centermike
+
+var extranotes
+
