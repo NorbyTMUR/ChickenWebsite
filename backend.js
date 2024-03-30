9b5a6204e3c2212526a3dda874dd67e5a8b5b6dd
NorbyTMUR <anjalithesamurai@gmail.com>
2024-03-29 23:11:49 -0500

    Issues: qrcode has problems, no stepper yet, no team number input/match number input yet, no login yet

--- backend.js
+++ backend.js
@@ -1,3 +1,5 @@
+    //imported a qrcode library.
+    src= "https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js"
     //also should input these booleans into the json
     var blueAlliance = new Boolean(false);
     var redAlliance = new Boolean(false);
@@ -19,11 +21,11 @@
         console.log(redAlliance);
     }
 
-
-    var isChecked = new Boolean(false);
+    //use for checkbox class only
     function fnChecked(elementName) {
-        isChecked = document.getElementById(elementName).checked;
+        let isChecked = document.getElementById(elementName).checked;
         console.log(isChecked);
+        return isChecked == true ? 1:0
     }
 
 
@@ -31,41 +33,58 @@
     /**
      * @precondition takes a string name
      */
-    let pts = 0;
+    
     function pointCounter(name){
-        pts = document.getElementById(name).value;
+        let pts = document.getElementById(name).value;
+        if(pts == null){
+            pts=0;
+        }
         console.log(pts);
+        return pts;
     }
     
     // JavaScript to handle toggle switch state change and display text accordingly
     //TODO: Figure out how to collect the data in a json file (turns into a qrcode)
     
-    const toggleInput = document.getElementById('allianceBlue');
-    jsobj = {}
-    jsobj['alliance'] = toggleInput
-    console.log("Here I am on my own....");
+    function generateQr(input) { 
+        let qrcode = new QRCode(document.querySelector(".qrcode")); 
 
+        qrcode.makeCode( 
+            document.querySelector( 
+                input
+            ).value); 
+    }
+    
     function collectData(){
-        fnChecked("leftZone") *2;
-        pointCounter("autoAmpPts") * 2;
-        pointCounter("autoSpPts") * 5;
-        pointCounter("teleAmpPts");
-        pointCounter("teleSpPts") * 2;
-        pointCounter("teleSpAmplifiedPts") * 5;
+        let jsobj = {}
+        jsobj['leftZone'] = fnChecked("leftZone") *2;
+        jsobj["autoAmpPts"] = pointCounter("autoAmpPts") * 2;
+        jsobj["autoSpPts"] = pointCounter("autoSpPts") * 5;
+        jsobj["teleAmpPts"] = pointCounter("teleAmpPts");
+        jsobj["teleSpPts"] = pointCounter("teleSpPts") * 2;
+        jsobj["teleSpAmplifiedPts"] = pointCounter("teleSpAmplifiedPts") * 5;
 
-        pointCounter("passes");
-        pointCounter("drops");
+        jsobj["passes"] = pointCounter("passes");
+        jsobj["drops"] = pointCounter("drops");
 
-        fnChecked("climbed") * 3;
-        fnChecked("parked");
-        fnChecked("traps");
+        jsobj["climbed"] = fnChecked("climbed") * 3;
+        jsobj["parked"] = fnChecked("parked");
+        jsobj["traps"] = fnChecked("traps");
 
-        pointCounter("trapsNum") * 5;
+        jsobj["trapsNum"] = pointCounter("trapsNum") * 5;
 
-        fnChecked("offeredcoop");
-        fnChecked("didcoop");
-        fnChecked("ampmike");
-        fnChecked("sourcemike");
-        fnChecked("centermike");
+        jsobj["offeredcoop"] = fnChecked("offeredcoop");
+        jsobj["didcoop"] = fnChecked("didcoop");
+        //maybe create a further divider for this, or have these appear if the human 
+        //player made their throw.
+        jsobj["ampmike"] = fnChecked("ampmike");
+        jsobj["sourcemike"] = fnChecked("sourcemike");
+        jsobj["centermike"] = fnChecked("centermike");
+        console.log(JSON.stringify(jsobj))
+        generateQr(JSON.stringify(jsobj));
+        
+        console.log("Here I am on my own....");
         
     }
+
+   
